469154413e162b9786b00fa92bd7ac759a375613
Tim Smith <tim@cozy.co>
2015-05-30 13:32:43 -0700

    Update version library

--- lib/sensu-plugins-openvpn.rb
+++ lib/sensu-plugins-openvpn.rb
@@ -1,7 +1,15 @@
+
+require 'sensu-plugins-openvpn/version'
+
+# Load the defaults
+
 #
-# Set gem version
+# Default class
 #
 module SensuPluginsOpenvpn
-  # Gem version
-  VERSION = '0.0.1.alpha.1'
+  class << self
+  end
+
+  class << self
+  end
 end
